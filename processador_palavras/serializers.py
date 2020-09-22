f60f7c0d987e10c055bc637c2ce7bdfd3495de5a
João Miranda <jvdmmiranda@gmail.com>
2020-09-22 17:09:04 -0300

    Create serializer for adding texts on document

--- processador_palavras/serializers.py
+++ processador_palavras/serializers.py
@@ -1,2 +1,9 @@
 from rest_framework import serializers
 from .models import *
+
+
+class DocumentTextSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Document
+        fields = ['text',]
