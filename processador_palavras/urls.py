3965f3c4c2bdde56511cde35db5440e0c3895974
João Miranda <jvdmmiranda@gmail.com>
2020-09-22 17:17:56 -0300

    Fixes urls file

--- processador_palavras/urls.py
+++ processador_palavras/urls.py
@@ -7,3 +7,9 @@ router = routers.DefaultRouter()
 router.register('documento', views.DocumentViewSet, basename="processador-palavras")
 router.register('vocabulario-default', views.SequenceDefaultViewSet, basename="vocabulario-default")
 router.register('vocabulario-grupo', views.Sequence2GramViewSet, basename="vocabulario-grupo")
+
+app_name = "processador-palavras"
+urlpatterns = [
+    path(r'', include(router.urls))
+]
+urlpatterns += router.urls
