c003c34aa36f40ff8a3ff6f5e976918cd8270956
João Miranda <jvdmmiranda@gmail.com>
2020-09-22 17:12:12 -0300

    Adds serialization for nxn relationships

--- processador_palavras/serializers.py
+++ processador_palavras/serializers.py
@@ -7,3 +7,17 @@ class DocumentTextSerializer(serializers.ModelSerializer):
     class Meta:
         model = Document
         fields = ['text',]
+
+
+class DocumentSequenceDefaultSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = DocumentSequenceDefault
+        fields = '__all__'
+
+
+class DocumentSequence2GramsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = DocumentSequence2Grams
+        fields = '__all__'
